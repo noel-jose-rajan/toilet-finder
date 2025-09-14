312d26586b2ce48cbb41eb0f0ee08bc205800378
noel <noeljose@protonmail.com>
2025-09-14 23:37:38 +0530

    backup

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -15,57 +15,57 @@ import { useLiveLocation } from '../../hooks/useLiveLocation';
 import MapMarker from 'react-native-maps/src/MapMarker';
 
 export default function Map() {
-  const { granted, requestPermission } = useLocationPermission();
-  const { location, error, loading } = useLiveLocation(!!granted);
+  // const { granted, requestPermission } = useLocationPermission();
+  // const { location, error, loading } = useLiveLocation(!!granted);
 
   const mapRef = useRef<MapView | null>(null);
 
-  // Request permission automatically on mount
-  useEffect(() => {
-    requestPermission();
-  }, []);
+  // // Request permission automatically on mount
+  // useEffect(() => {
+  //   requestPermission();
+  // }, []);
 
-  useEffect(() => {
-    if (location.latitude != null && location.longitude !== null) {
-      mapRef.current?.animateToRegion({
-        latitude: location.latitude,
-        longitude: location.longitude,
-        latitudeDelta: 0.005,
-        longitudeDelta: 0.005,
-      });
-    }
-  }, [location]);
+  // useEffect(() => {
+  //   if (location.latitude != null && location.longitude !== null) {
+  //     mapRef.current?.animateToRegion({
+  //       latitude: location.latitude,
+  //       longitude: location.longitude,
+  //       latitudeDelta: 0.005,
+  //       longitudeDelta: 0.005,
+  //     });
+  //   }
+  // }, [location]);
 
-  if (!granted) {
-    return (
-      <View style={styles.center}>
-        <Text>No permission granted</Text>
-        <Button
-          title="Open Settings"
-          color="black"
-          onPress={() => Linking.openSettings()}
-        />
-      </View>
-    );
-  }
+  // if (!granted) {
+  //   return (
+  //     <View style={styles.center}>
+  //       <Text>No permission granted</Text>
+  //       <Button
+  //         title="Open Settings"
+  //         color="black"
+  //         onPress={() => Linking.openSettings()}
+  //       />
+  //     </View>
+  //   );
+  // }
 
-  if (loading) {
-    return (
-      <View style={styles.center}>
-        <ActivityIndicator size="large" />
-        <Text>Fetching location...</Text>
-      </View>
-    );
-  }
+  // if (loading) {
+  //   return (
+  //     <View style={styles.center}>
+  //       <ActivityIndicator size="large" />
+  //       <Text>Fetching location...</Text>
+  //     </View>
+  //   );
+  // }
 
-  if (error) {
-    return (
-      <View style={styles.center}>
-        <Text>Error: {error.message}</Text>
-        <Button title="Retry" onPress={requestPermission} />
-      </View>
-    );
-  }
+  // if (error) {
+  //   return (
+  //     <View style={styles.center}>
+  //       <Text>Error: {error.message}</Text>
+  //       <Button title="Retry" onPress={requestPermission} />
+  //     </View>
+  //   );
+  // }
 
   return (
     <View style={{ flex: 1 }}>
@@ -73,17 +73,24 @@ export default function Map() {
         ref={mapRef}
         provider={PROVIDER_GOOGLE}
         style={StyleSheet.absoluteFillObject}
+        initialRegion={{
+          latitude: 12, // same as your marker
+          longitude: 12,
+          latitudeDelta: 1,
+          longitudeDelta: 1,
+        }}
         customMapStyle={blackAndWhiteMapStyle}
       >
-        {location.latitude != null && location.longitude !== null && (
-          <MapMarker
-            image={require('../../../assets/pee-marker.png')}
+        {
+          // location.latitude != null && location.longitude !== null &&
+          <Marker
             coordinate={{
-              latitude: location.latitude,
-              longitude: location.longitude,
+              latitude: 12,
+              longitude: 12,
             }}
+            image={require('../../../assets/location-50.png')}
           />
-        )}
+        }
       </MapView>
     </View>
   );
